ebb50e28bdb747831f918619fe337210df84786e
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2017-11-16 11:15:01 +0000

    Fix metadata of test cookbook.

--- test/coobooks/vault_certificate_test/metadata.rb
+++ test/coobooks/vault_certificate_test/metadata.rb
@@ -1,10 +1,4 @@
 name 'vault_certificate_test'
-
-maintainer 'Simão Martins'
-maintainer_email 'simao.martins@tecnico.ulisboa.pt'
-
+version '1.0.0'
 license 'Apache-2.0'
-
-description 'This cookbook is used with test-kitchen to test vault_certificate cookbook.'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 depends 'vault_certificate'

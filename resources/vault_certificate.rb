c168610d8d8fdf4a620b981bb054b413d1fa9109
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2021-01-06 20:02:11 +0000

    Check for the data after.

--- resources/vault_certificate.rb
+++ resources/vault_certificate.rb
@@ -173,8 +173,10 @@ action_class do
              end
     raise ArgumentError, "[vault-certificate] Vault returned nil for path '#{new_resource.vault_path}' and options #{new_resource.options}" if result.nil?
 
+    ssl_item = deep_copy(result.data)
     # The if makes it possible to read from KV version 2.
-    ssl_item = deep_copy(result.data.key?(:data) ? result.data[:data] : result.data)
+    ssl_item = ssl_item.key?(:data) ? ssl_item[:data] : ssl_item
+    
     missing_items = []
     missing_items += ['certificate'] if ssl_item[:certificate].nil?
     missing_items += ['chain'] if ssl_item[:ca_chain].nil? && ssl_item[:issuing_ca].nil?
